9e4899d97d16d1da071cb9b1e7da283038ac13a1
Amedveddev <51256469+Amedveddev@users.noreply.github.com>
2020-08-02 12:39:08 +0300

    Add files via upload

---
+++ docs/precache-manifest.52f7975912710ab552d9f1ad666e5d50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be4aaae2faee894cbaf21b739dd65ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73be1967b38a89e9064f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fc1a3779ac8d616b1d81",
+    "url": "/static/css/main.5184f4ae.chunk.css"
+  },
+  {
+    "revision": "73be1967b38a89e9064f",
+    "url": "/static/js/2.a089a135.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a089a135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1a3779ac8d616b1d81",
+    "url": "/static/js/main.94b85168.chunk.js"
+  },
+  {
+    "revision": "9c38578d78ee052cc19c",
+    "url": "/static/js/runtime-main.aa6e1cf3.js"
+  }
+]);
